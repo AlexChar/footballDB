d5a52022ffccb06721bf1a8c7457d60713e15ff1
AlexChar <alex.charizopoulos@gmail.com>
2015-05-16 17:50:41 +0300

    project scaffolding

---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('footballDb', ['ngAnimate', 'ngCookies', 'ngSanitize', 'ngResource', 'ngRoute'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+;
---
+++ src/app/main/main.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('footballDb')
+  .controller('MainCtrl', function ($scope) {
+    $scope.name = 'alex';
+  });
